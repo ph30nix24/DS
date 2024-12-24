6d83fef393ae598d7c9180db0825af8ff1ef6daf
Anuj <starlordindia1230@gmail.com>
2024-12-24 19:38:27 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode basename={import.meta.env.BASE_URL}>
-    <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
